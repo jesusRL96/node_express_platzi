31d0d7d7b710606a064c176e956074dab856f0a9
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-01-31 13:38:51 -0600

    Routing con Express.js

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
     res.send('Hola')
 });
 
+app.get('/nueva-ruta', (req, res) => {
+    res.send('Nueva ruta')
+});
+
+app.get('/products', (req, res) => {
+    res.json({
+        name: 'Product 100',
+        price: 200
+    })
+});
+
 app.listen(port, ()=>{
     console.log('port: ' + port);
 });
