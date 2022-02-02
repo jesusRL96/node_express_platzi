6896b650c3beee0001396641987f685b8b8cc134
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-02-02 13:21:01 -0600

    Separación de responsabilidades con express.Router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const faker = require('faker');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
@@ -12,54 +13,7 @@ app.get('/nueva-ruta', (req, res) => {
     res.send('Nueva ruta')
 });
 
-app.get('/products/', (req, res) => {
-    const products = [];
-    const { size } = req.query;
-    const limit = size || 10;
-    for (let index = 0; index < limit; index++) {
-        products.push({
-            name: faker.commerce.productName(),
-            price: parseInt(faker.commerce.price(), 10),
-            image: faker.image.imageUrl()
-        })
-
-    }
-    res.json(products)
-});
-
-app.get('/products/filter', (req, res) => {
-    res.send('hola filter')
-});
-
-app.get('/products/:id', (req, res) => {
-    const {id} = req.params;
-    res.json({
-            id,
-            name: 'Product 1',
-            price: 100
-        })
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-    const {categoryId, productId} = req.params;
-    res.json({
-            categoryId,
-            productId,
-        })
-});
-
-app.get('/users', (req, res) => {
-    const {limit, offset} = req.query
-    if (limit && offset) {
-        res.json({
-            limit,
-            offset
-        })
-    }
-    else {
-        res.send('no hay parametros')
-    }
-});
+routerApi(app);
 
 app.listen(port, ()=>{
     console.log('port: ' + port);
