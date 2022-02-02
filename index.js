dd587237590a37611d37d06d6de65c1b9bdebef1
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-02-02 12:38:13 -0600

    GET: parámetros query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -12,16 +13,22 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products/', (req, res) => {
-    res.json([
-            {
-            name: 'Product 1',
-            price: 100
-        },
-            {
-            name: 'Product 2',
-            price: 200
-        }
-    ])
+    const products = [];
+    const { size } = req.query;
+    const limit = size || 10;
+    for (let index = 0; index < limit; index++) {
+        products.push({
+            name: faker.commerce.productName(),
+            price: parseInt(faker.commerce.price(), 10),
+            image: faker.image.imageUrl()
+        })
+
+    }
+    res.json(products)
+});
+
+app.get('/products/filter', (req, res) => {
+    res.send('hola filter')
 });
 
 app.get('/products/:id', (req, res) => {
@@ -39,6 +46,19 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
             categoryId,
             productId,
         })
+});
+
+app.get('/users', (req, res) => {
+    const {limit, offset} = req.query
+    if (limit && offset) {
+        res.json({
+            limit,
+            offset
+        })
+    }
+    else {
+        res.send('no hay parametros')
+    }
 });
 
 app.listen(port, ()=>{
