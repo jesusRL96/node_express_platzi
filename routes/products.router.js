69df252e8854e2e299390766a0dab1bc1348c3d5
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-02-10 16:17:00 -0600

    DELETE, PATCH, PATCH

--- routes/products.router.js
+++ routes/products.router.js
@@ -39,4 +39,23 @@ router.post('/', (req, res) => {
     });
 });
 
+// router.update('/:id', (req, res) => {
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+    res.json({
+        message:'created',
+        data: body,
+        id,
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({
+        message:'deleted',
+        id,
+    });
+});
+
 module.exports = router;
